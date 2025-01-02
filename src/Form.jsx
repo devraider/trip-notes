c871ff69589429e0bc62f05150e6c5803165d20d
Paul Cristea <cristeapaul.2015@gmail.com>
2025-01-03 00:39:47 +0200

    feat(Form): Created form fields

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,19 @@
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
   return (
-    <div className="add-form">
-      <h3>What do you need?</h3>
-    </div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip? 😍</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((i) => (
+          <option value={i}>{i}</option>
+        ))}
+      </select>
+      <input type="text" placeholder="Add item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
