7d60822b4d9fe38c5d56919b163ca183a9f26d82
Paul Cristea <cristeapaul.2015@gmail.com>
2025-01-03 11:05:23 +0200

    feat(PackingList): Added ids to items

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -2,18 +2,22 @@ import Item from "./Item";
 
 const items = [
   {
+    id: 1,
     quantity: 1,
     description: "Passport",
   },
   {
+    id: 3,
     quantity: 1,
     description: "Passport",
   },
   {
-    quantity: 1,
+    id: 2,
+    quanity: 1,
     description: "Passport",
   },
   {
+    id: 4,
     quantity: 1,
     description: "Passport",
     packed: true,
@@ -24,7 +28,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item key={item.name} item={item} />
+          <Item key={item.id} item={item} />
         ))}
       </ul>
     </div>
