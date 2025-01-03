f1eb1572172153a8b4af53b6a7a6ea570131998d
Paul Cristea <cristeapaul.2015@gmail.com>
2025-01-03 15:10:51 +0200

    feat(PackingList): Derived state for sorting

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Item from "./Item";
 
 function PackingList({ items, onRemoveItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -13,6 +30,13 @@ function PackingList({ items, onRemoveItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort input order</option>
+          <option value="description">Sort description</option>
+          <option value="packed">Sort packed</option>
+        </select>
+      </div>
     </div>
   );
 }
