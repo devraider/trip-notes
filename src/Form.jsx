b85011f50ca5e16b8740fa3d34ba189e1247713e
Paul Cristea <cristeapaul.2015@gmail.com>
2025-01-03 11:09:10 +0200

    feat(Form): Added Controlled Items

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,17 +1,46 @@
+import { useState } from "react";
+
 function Form() {
+  const [description, setDescription] = useState("");
+  const [itemCount, setItemCount] = useState(2);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    // Guard to provent empty value fro input field
+    if (!description) {
+      return;
+    }
+
+    const newItem = {
+      description,
+      itemCount,
+      packed: false,
+      id: Date.now(),
+    };
+
+    setDescription("");
+    setItemCount(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-      <select>
+      <select
+        onChange={(e) => setItemCount(Number(e.target.value))}
+        value={itemCount}
+      >
         {Array.from({ length: 20 }, (_, index) => index + 1).map((i) => (
-          <option value={i}>{i}</option>
+          <option key={i} value={i}>
+            {i}
+          </option>
         ))}
       </select>
-      <input type="text" placeholder="Add item..." />
+      <input
+        type="text"
+        placeholder="Add item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
